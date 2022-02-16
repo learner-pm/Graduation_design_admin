1ec5a676032906746adbc788814f8e7ceb846f7c
pm <mao.peng@ncsi.com.cn>
2022-02-16 10:55:20 +0800

    update login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -9,13 +9,14 @@ import {
   CheckOutlined,
   MailOutlined,
   KeyOutlined,
+  ExclamationOutlined,
 } from '@ant-design/icons';
 import './style/login.less';
 
 const Login = (props) => {
   const url = require('./image/login.jpg');
-  const [email, setEmail] = useState('');
-  const [word, setWord] = useState('');
+  const [email, setEmail] = useState(undefined);
+  const [word, setWord] = useState(undefined);
   const getEmail = (e) => {
     setEmail(e.target.value);
     console.log(e.target.value);
@@ -23,6 +24,7 @@ const Login = (props) => {
   const getWord = (e) => {
     setWord(e.target.value);
   };
+  console.log(typeof typeof word);
   const login = () => {
     if (email && word) {
       sessionStorage.setItem('id', '001');
@@ -33,55 +35,87 @@ const Login = (props) => {
     console.log(`checked = ${e.target.checked}`);
   }
   return (
-    <div className="login">
-      <div className="login_title">
-        <p>Graduation Design</p>
-      </div>
-      <div className="login_content">
-        <div className="login_image">
-          {/* <img src={url} alt="" srcset="" /> */}
+    <div className="layout">
+      <div className="login">
+        <div className="login_left">
+          <p>Graduation Design</p>
+          <div></div>
         </div>
-        <div className="login_sign">
-          <p className="login_title">Welcome Back:)</p>
-          <p className="login_des">
-            This is the background management part of my graduation project,
-            Details please see <a href="https://pmthank.cn/">here</a>.
-          </p>
-          <div className="login_sign_content">
-            <div className="sign_div">
-              <span className="sign_div_type">
-                <MailOutlined />
-              </span>
-              <div className="sign_input">
-                <p>Email</p>
-                <input type="text" value={email} onChange={getEmail} />
-              </div>
-              <div className="login_sign_status">
-                <CheckOutlined />
-              </div>
-            </div>
-            <div className="sign_div">
-              <span className="sign_div_type">
-                <KeyOutlined />
-              </span>
-              <div className="sign_input">
-                <p>PassWord</p>
-                <input type="password" value={word} onChange={getWord} />
-              </div>
-              <div className="login_sign_status">
-                <CloseOutlined />
-              </div>
-            </div>
-          </div>
-          <div className="login_remk">
-            <p>
-              <Checkbox onChange={onChange}>Remember Me</Checkbox>
+        <div className="login_content">
+          {/* <div className="login_image">
+          <img src={url} alt="" srcset="" />
+        </div> */}
+          <div className="login_sign">
+            <p className="login_title">Welcome Back:)</p>
+            <p className="login_des">
+              This is the background management part of my graduation project,
+              Details please see{' '}
+              <a href="https://pmthank.cn/" target="_blank">
+                here
+              </a>
+              .
             </p>
-            <p>Forget PassWord</p>
-          </div>
-          <div className="login_button">
-            <button onClick={login}>Sign in</button>
-            <button>Create Account</button>
+            <div className="login_sign_content">
+              <div className="sign_div">
+                <span className="sign_div_type">
+                  <MailOutlined />
+                </span>
+                <div className="sign_input">
+                  <p>Email</p>
+                  <input type="text" value={email} onChange={getEmail} />
+                </div>
+                {typeof email !== 'undefined' ? (
+                  email === '' ? (
+                    <div className="login_sign_status fail">
+                      <CloseOutlined />
+                    </div>
+                  ) : (
+                    <div className="login_sign_status suc">
+                      <CheckOutlined />
+                    </div>
+                  )
+                ) : (
+                  <div className="login_sign_status pad">
+                    <ExclamationOutlined />
+                  </div>
+                )}
+              </div>
+              <div className="sign_div">
+                <span className="sign_div_type">
+                  <KeyOutlined />
+                </span>
+                <div className="sign_input">
+                  <p>PassWord</p>
+                  <input type="password" value={word} onChange={getWord} />
+                </div>
+
+                {typeof word !== 'undefined' ? (
+                  word === '' ? (
+                    <div className="login_sign_status fail">
+                      <CloseOutlined />
+                    </div>
+                  ) : (
+                    <div className="login_sign_status suc">
+                      <CheckOutlined />
+                    </div>
+                  )
+                ) : (
+                  <div className="login_sign_status pad">
+                    <ExclamationOutlined />
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="login_remk">
+              <p>
+                <Checkbox onChange={onChange}>Remember Me</Checkbox>
+              </p>
+              <p>Forget PassWord</p>
+            </div>
+            <div className="login_button">
+              <button onClick={login}>Sign in</button>
+              <button>Create Account</button>
+            </div>
           </div>
         </div>
       </div>
